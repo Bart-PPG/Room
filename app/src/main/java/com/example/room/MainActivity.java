69e56e6a35e8f3af1dc3e662213b0799131f6cd8
bart-ppg <bartek.nitek@onet.pl>
2025-03-26 13:09:42 +0100

    przygotowanie

---
+++ app/src/main/java/com/example/room/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.room;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//1. klasa opisujaca pojedynce rekordy / obiekty
+//2.Do klasy adnotacje bazo danowe
